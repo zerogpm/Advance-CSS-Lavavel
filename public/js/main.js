f06e87eb1d7abb00472ae83d0a305a0ec296911a
zerogpm <zerogpm@gmail.com>
2018-09-09 22:41:46 -0700

    add and remove css class

--- public/js/main.js
+++ public/js/main.js
@@ -83,22 +83,36 @@ var backdrop = document.querySelector('.backdrop');
 var selectPlanButtons = document.querySelectorAll('.plan button');
 var modal = document.querySelector('.modal');
 var closeButton = document.querySelector('.modal__action--negative');
-//console.dir(yesButton);
+var toggleButton = document.querySelector('.toggle-button');
+var mobileNav = document.querySelector('.mobile-nav');
+console.dir(mobileNav);
 for (var i = 0; i < selectPlanButtons.length; i++) {
     selectPlanButtons[i].addEventListener('click', function () {
-        modal.style.display = 'block';
-        backdrop.style.display = 'block';
+        // modal.style.display = 'block';
+        // backdrop.style.display = 'block';
+        modal.classList.add('open');
+        backdrop.classList.add('open');
     });
 }
 
-backdrop.addEventListener('click', closeModal);
+backdrop.addEventListener('click', function () {
+    mobileNav.classList.remove('open');
+    backdrop.classList.remove('open');
+});
 
 closeButton.addEventListener('click', closeModal);
 
 function closeModal() {
-    modal.style.display = 'none';
-    backdrop.style.display = 'none';
+    // modal.style.display = 'none';
+    // backdrop.style.display = 'none';
+    modal.classList.remove('open');
+    backdrop.classList.remove('open');
 }
+
+toggleButton.addEventListener('click', function () {
+    mobileNav.classList.add('open');
+    backdrop.classList.add('open');
+});
 
 /***/ }),
 /* 2 */
--- resources/assets/js/main.js
+++ resources/assets/js/main.js
@@ -2,19 +2,33 @@ let backdrop = document.querySelector('.backdrop');
 let selectPlanButtons = document.querySelectorAll('.plan button');
 let modal = document.querySelector('.modal');
 let closeButton = document.querySelector('.modal__action--negative');
-//console.dir(yesButton);
+let toggleButton = document.querySelector('.toggle-button');
+let mobileNav = document.querySelector('.mobile-nav');
+console.dir(mobileNav);
 for (let i = 0; i < selectPlanButtons.length; i++) {
     selectPlanButtons[i].addEventListener('click', () => {
-        modal.style.display = 'block';
-        backdrop.style.display = 'block';
+        // modal.style.display = 'block';
+        // backdrop.style.display = 'block';
+        modal.classList.add('open');
+        backdrop.classList.add('open');
     });
 }
 
-backdrop.addEventListener('click', closeModal);
+backdrop.addEventListener('click', () => {
+    mobileNav.classList.remove('open');
+    backdrop.classList.remove('open');
+});
 
 closeButton.addEventListener('click', closeModal);
 
 function closeModal() {
-    modal.style.display = 'none';
-    backdrop.style.display = 'none';
+    // modal.style.display = 'none';
+    // backdrop.style.display = 'none';
+    modal.classList.remove('open');
+    backdrop.classList.remove('open');
 }
+
+toggleButton.addEventListener('click', () => {
+    mobileNav.classList.add('open');
+    backdrop.classList.add('open');
+});
